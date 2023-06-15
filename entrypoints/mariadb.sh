d5af33cbd851d536d6c074808c8b2cbe6068601c
ly020044 <ly020044@gmail.com>
2023-06-15 08:56:20 +0800

    添加文件 docker-compose 和 sample.settings.json

--- entrypoints/mariadb.sh
+++ entrypoints/mariadb.sh
@@ -192,7 +192,7 @@ create_dot_my_cnf
 [[ "${_run_pre_start}" == "YES" ]] && run_scripts_in_dir ${PRE_START_SCRIPTS_DIR}
 
 # Generate all config files with custom settings.
-/gosible/gosible -e /gosible/settings.zhb.json -p docker.yml
+/gosible/gosible -e /root/.iredmail/settings.json -p docker.yml
 
 stop_temp_mysql_instance
 
