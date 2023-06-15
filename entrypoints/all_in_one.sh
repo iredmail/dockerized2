13525e5ceed072f3f1b08f94a399bfd947c407a0
Zhang Huangbin <zhb@iredmail.org>
2023-06-15 11:45:14 +0800

    Fix missing import.

--- entrypoints/all_in_one.sh
+++ entrypoints/all_in_one.sh
@@ -3,6 +3,8 @@
 # Remove leftover pid files which may cause service fail to start.
 find /run -name "*.pid" | xargs rm -f {}
 
+. /docker/entrypoints/functions.sh
+
 run_entrypoint ${ENTRYPOINTS_DIR}/mariadb.sh
 #run_entrypoint ${ENTRYPOINTS_DIR}/rsyslog.sh
 #run_entrypoint ${ENTRYPOINTS_DIR}/cron.sh
