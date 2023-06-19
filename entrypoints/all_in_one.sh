d5db4ea146f135bc7e62326d22e44385b004f30e
Zhang Huangbin <zhb@iredmail.org>
2023-06-19 18:02:57 +0800

    Remove unnecessary files.

--- entrypoints/all_in_one.sh
+++ entrypoints/all_in_one.sh
@@ -3,22 +3,27 @@
 # Remove leftover pid files which may cause service fail to start.
 find /run -name "*.pid" | xargs rm -f {}
 
-ENTRYPOINTS_DIR="/docker/entrypoints"
-. ${ENTRYPOINTS_DIR}/functions.sh
+# System accounts.
+export SYS_USER_ROOT="root"
+export SYS_GROUP_ROOT="root"
+export SYS_USER_SYSLOG="syslog"
+export SYS_GROUP_SYSLOG="adm"
+export SYS_USER_NGINX="www-data"
+export SYS_GROUP_NGINX="www-data"
+export SYS_USER_MYSQL="mysql"
+export SYS_GROUP_MYSQL="mysql"
+export SYS_USER_CLAMAV="clamav"
+export SYS_GROUP_CLAMAV="clamav"
 
-run_entrypoint ${ENTRYPOINTS_DIR}/mariadb.sh
-#run_entrypoint ${ENTRYPOINTS_DIR}/rsyslog.sh
-#run_entrypoint ${ENTRYPOINTS_DIR}/cron.sh
+# Commands.
+CMD_PERL="perl -pi -e"
+
+. /docker/entrypoints/mariadb.sh
 
 install -d -m 0755 /var/run/supervisord /var/log/supervisor
 install -d -o ${SYS_USER_SYSLOG} -g ${SYS_GROUP_SYSLOG} -m 0755 /var/log/php-fpm
 install -d -o ${SYS_USER_NGINX}  -g ${SYS_GROUP_NGINX}  -m 0755 /run/php
 install -d -o ${SYS_USER_CLAMAV} -g ${SYS_GROUP_CLAMAV} -m 0755 /run/clamav/
-
-SUP_SERVICES="cron rsyslog mariadb dovecot postfix mlmmjadmin iredapd clamav amavisd nginx phpfpm fail2ban iredadmin"
-for srv in ${SUP_SERVICES}; do
-    ln -sf /etc/supervisor/conf-available/${srv}.conf /etc/supervisor/conf.d/${srv}.conf
-done
 
 # Run specified commands in Dockerfile `CMD`.
 LOG "CMD: $@"
--- entrypoints/mariadb.sh
+++ entrypoints/mariadb.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 # Author: Zhang Huangbin <zhb@iredmail.org>
 
-#
-# This file is managed by iRedMail Team <support@iredmail.org> with Ansible,
-# please do __NOT__ modify it manually.
-#
-
-# TODO
-#   - Delete anonymous user
-#   - Drop 'test' database
-
-. /docker/entrypoints/functions.sh
+SYS_USER_MYSQL="mysql"
+SYS_GROUP_MYSQL="mysql"
 
 DATA_DIR="/var/lib/mysql"
 CUSTOM_CONF_DIR="/opt/iredmail/custom/mysql"
 SOCKET_PATH="/run/mysqld/mysqld.sock"
 DOT_MY_CNF="/root/.my.cnf"
 
-
-# Directories used to store pre-start and initialization shell/sql scripts.
-#PRE_START_SCRIPTS_DIR="/docker/mariadb/pre_start"
-
-#require_non_empty_var MYSQL_ROOT_PASSWORD ${MYSQL_ROOT_PASSWORD}
-#require_non_empty_var VMAIL_DB_PASSWORD ${VMAIL_DB_PASSWORD}
-#require_non_empty_var VMAIL_DB_ADMIN_PASSWORD ${VMAIL_DB_ADMIN_PASSWORD}
-
 # Add required directories.
 if [[ ! -d ${CUSTOM_CONF_DIR} ]]; then
-    LOG "Create directory used to store custom config files: ${CUSTOM_CONF_DIR}".
+    echo "Create directory used to store custom config files: ${CUSTOM_CONF_DIR}".
     mkdir -p ${CUSTOM_CONF_DIR}
 fi
 
@@ -35,15 +19,10 @@ fi
 [[ -d ${DATA_DIR} ]] || mkdir -p ${DATA_DIR}
 
 _first_run="NO"
-_run_pre_start="NO"
 
 if [[ ! -d "${DATA_DIR}/mysql" ]]; then
     _first_run="YES"
 fi
-
-#if [[ -d "${PRE_START_SCRIPTS_DIR}" ]] && [[ "$(ls -A ${PRE_START_SCRIPTS_DIR})" ]]; then
-#    _run_pre_start="YES"
-#fi
 
 cmd_mysql_opts="--protocol=socket -uroot -hlocalhost --socket=${SOCKET_PATH}"
 cmd_mysql="mysql ${cmd_mysql_opts}"
@@ -56,13 +35,13 @@ if [[ X"${_first_run}" != X'YES' ]]; then
 fi
 
 start_temp_mysql_instance() {
-    LOG "Starting temporary MariaDB instance."
+    echo "Starting temporary MariaDB instance."
     mysqld ${cmd_mysqld_opts} &
     _pid="$!"
     echo "${_pid}" > /tmp/temp_instance_pid
 
     # Wait until MariaDB instance is started
-    #LOG "Waiting for MariaDB service ..."
+    #echo "Waiting for MariaDB service ..."
     for i in $(seq 30); do
         if mysqladmin --socket="${SOCKET_PATH}" ping &>/dev/null; then
             break
@@ -71,23 +50,23 @@ start_temp_mysql_instance() {
         sleep 1
 
         if [[ "$i" == 30 ]]; then
-            LOG "Initialization failed. Please check ${DATA_DIR}/mysqld.err for more details."
+            echo "Initialization failed. Please check ${DATA_DIR}/mysqld.err for more details."
             exit 255
         fi
     done
 }
 
 stop_temp_mysql_instance() {
-    LOG "Stopping the temporary mysql instance."
+    echo "Stopping the temporary mysql instance."
     _pid="$(cat /tmp/temp_instance_pid)"
 
     if ! kill -s TERM "${_pid}" || ! wait "${_pid}"; then
-        LOG_ERROR "Failed to stop temporary MariaDB instance."
+        echo "Failed to stop temporary MariaDB instance."
         exit 255
     fi
 
     rm -f /tmp/temp_instance_pid
-    LOG "Stopped the temporary mysql instance."
+    echo "Stopped the temporary mysql instance."
 }
 
 create_root_user() {
@@ -115,82 +94,38 @@ EOF
         _cmd_mysql="${cmd_mysql}"
     fi
 
-    LOG "Create MariaDB root user."
+    echo "Create MariaDB root user."
     sh -c "${_cmd_mysql} < ${_file}"
     rm -f ${_file}
 }
 
-reset_password() {
-    _user="$1"
-    _host="$2"
-    _pw="$3"
-
-    LOG "Reset password for SQL user '${_user}'%'${_host}'."
-    mysql -u root --socket=${SOCKET_PATH} <<EOF
-FLUSH PRIVILEGES;
-ALTER USER '${_user}'@'${_host}' IDENTIFIED BY '${_pw}';
-FLUSH PRIVILEGES;
-EOF
-}
-
-create_dot_my_cnf() {
-    cat <<EOF > ${DOT_MY_CNF}
-[client]
-host=localhost
-user=root
-password="${MYSQL_ROOT_PASSWORD}"
-socket="${SOCKET_PATH}"
-EOF
-
-}
-
-# Run all .sh/.sql/.sql.gz scripts in given directory.
-run_scripts_in_dir() {
-    _dir="${1}"
-
-    if [[ -f ${DOT_MY_CNF} ]]; then
-        _cmd_mysql="${cmd_mysql_with_dot_cnf}"
-    else
-        _cmd_mysql="${cmd_mysql}"
-    fi
-
-    if [[ -d ${_dir} ]] && [[ "$(ls -A ${_dir})" ]]; then
-        for f in ${_dir}/*; do
-            case "$f" in
-                *.sh)
-                    LOG "[Run] $f"
-                    . "$f"
-                    ;;
-                *.sql)
-                    LOG "[Run] $f"
-                    sh -c "${_cmd_mysql}" < "$f"
-                    ;;
-                *.mysql) : ;;
-                *) LOG "[Ignore] $f. Only *.sh and *.sql are supported." ;;
-            esac
-        done
-    fi
-}
+#reset_password() {
+#    _user="$1"
+#    _host="$2"
+#    _pw="$3"
+#
+#    echo "Reset password for SQL user '${_user}'%'${_host}'."
+#    mysql -u root --socket=${SOCKET_PATH} <<EOF
+#FLUSH PRIVILEGES;
+#ALTER USER '${_user}'@'${_host}' IDENTIFIED BY '${_pw}';
+#FLUSH PRIVILEGES;
+#EOF
+#}
 
 # Create directory used to store socket/pid files.
 install -d -o ${SYS_USER_MYSQL} -g ${SYS_GROUP_MYSQL} -m 0755 $(dirname ${SOCKET_PATH})
 
 # Initialize database
 if [[ X"${_first_run}" == X'YES' ]]; then
-    LOG "Initializing database ..."
+    echo "Initializing database ..."
     mysql_install_db --user=${SYS_USER_MYSQL} --datadir=${DATA_DIR} >/dev/null
 fi
 
 # Start service since we always reset root password.
 start_temp_mysql_instance
 
-[[ X"${_first_run}" == X"YES" ]] && create_root_user
-[[ X"${_first_run}" != X"YES" ]] && reset_password root localhost ${MYSQL_ROOT_PASSWORD}
-
-# ~/.my.cnf is required by pre_start scripts.
-create_dot_my_cnf
-
-#[[ "${_run_pre_start}" == "YES" ]] && run_scripts_in_dir ${PRE_START_SCRIPTS_DIR}
+#[[ X"${_first_run}" == X"YES" ]] && create_root_user
+#[[ X"${_first_run}" != X"YES" ]] && reset_password root localhost ${MYSQL_ROOT_PASSWORD}
 
 # Generate all config files with custom settings.
 /gosible -e /root/.iredmail/settings.json -p docker.yml
