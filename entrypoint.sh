8289ba63c2b44e26120075baa1175c76bc92ca10
Zhang Huangbin <zhb@iredmail.org>
2023-06-22 12:00:46 +0800

    Run freshclam properly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,33 @@ while :; do
 done
 echo "MySQL service is up."
 
-install -d -o www-data -g www-data -m 0755 /run/php
-install -d -o clamav   -g clamav   -m 0755 /run/clamav/
-install -d -o syslog   -g adm      -m 0755 /var/log/php-fpm
-install -d -o root     -g root     -m 0755 /var/run/supervisord /var/log/supervisor
+CLAMAV_DB_DIR='/var/lib/clamav'
+install -d -o clamav -g clamav -m 0775 ${CLAMAV_DB_DIR}
+install -d -o clamav -g clamav -m 0755 /run/clamav/
+
+# Has clamav signature database.
+_has_sig=NO
+
+# Both bytecode.cvd and main.cvd are required.
+if [[ -f ${CLAMAV_DB_DIR}/bytecode.cvd ]] && [[ -f ${CLAMAV_DB_DIR}/main.cvd ]]; then
+    # Either daily.cvd or daily.cld is required.
+    if [[ -f ${CLAMAV_DB_DIR}/daily.cvd ]] || [[ -f ${CLAMAV_DB_DIR}/daily.cld ]]; then
+        _has_sig=YES
+    fi
+fi
+
+if [[ "${_has_sig}" == 'NO' ]]; then
+    echo "* No ClamAV signature database found, running freshclam..."
+    freshclam --verbose --user=clamav --datadir=/var/lib/clamav
+fi
 
 # Deploy all components.
 time /gosible -e /settings.json -p docker.yml
+
+install -d -o www-data -g www-data -m 0755 /run/php
+
+echo "* Run freshclam in background."
+freshclam --checks=1 --daemon --user=clamav --config-file=/etc/clamav/freshclam.conf
 
 # Run specified commands in Dockerfile `CMD`.
 echo "CMD: $@"
